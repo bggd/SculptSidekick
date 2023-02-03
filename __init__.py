4a3f352a60aef6aa29fb2a83016bd2ec62cbee61
bggd <gameovera@live.com>
2023-02-03 17:41:42 +0900

    separate dyntopo

--- __init__.py
+++ __init__.py
@@ -116,64 +116,6 @@ class SculptSidekickPanel(bpy.types.Panel):
             "sculpt_sidekick.flip_sidebar", text="Sidebar", depress=sidebar_is_flip
         )
 
-        toggle = layout.row()
-        toggle.prop(
-            sidekick,
-            "dyntopo_show",
-            icon="DOWNARROW_HLT" if sidekick.dyntopo_show else "RIGHTARROW",
-            text="",
-            emboss=False,
-        )
-        toggle.operator(
-            "sculpt.dynamic_topology_toggle",
-            text="Dyntopo",
-            icon="CHECKBOX_HLT" if is_active else "CHECKBOX_DEHLT",
-            emboss=True,
-        )
-
-        if sidekick.dyntopo_show:
-            method = layout.column()
-            method.active = is_active
-            method.prop(sculpt, "detail_type_method", expand=True)
-
-            cnst = layout.split(align=True, factor=0.9)
-            cnst.prop(sculpt, "constant_detail_resolution")
-            op_snpl = cnst.operator(
-                "sculpt.sample_detail_size", text="", icon="EYEDROPPER"
-            )
-            op_snpl.mode = "DYNTOPO"
-
-            rel = layout.column()
-            rel.prop(sculpt, "detail_size")
-
-            brush = layout.column()
-            brush.prop(sculpt, "detail_percent")
-
-            refine = layout.column()
-            refine.active = is_active
-            refine.prop(sculpt, "detail_refine_method", expand=True)
-
-            fill = layout.column()
-            fill.operator("sculpt.detail_flood_fill")
-
-            if (
-                sculpt.detail_type_method == "CONSTANT"
-                or sculpt.detail_type_method == "MANUAL"
-            ):
-                rel.active = False
-                brush.active = False
-            elif sculpt.detail_type_method == "RELATIVE":
-                cnst.active = False
-                brush.active = False
-            elif sculpt.detail_type_method == "BRUSH":
-                cnst.active = False
-                rel.active = False
-
-            if not is_active:
-                rel.active = False
-                cnst.active = False
-                brush.active = False
-
         wire = layout.row(align=True)
         wire.prop(context.space_data.overlay, "show_wireframes", text="")
         wire_opacity = wire.row()
@@ -195,6 +137,84 @@ class SculptSidekickPanel(bpy.types.Panel):
             wire_opacity.active = False
         if not context.space_data.overlay.show_wireframes:
             wire_opacity.active = False
+
+
+class SculptSidekickDyntopoPanel(bpy.types.Panel):
+    bl_idname = "UI_PT_SculptSidkickDyntopo"
+    bl_label = ""
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Sculpt Sidekick"
+
+    @classmethod
+    def poll(cls, context):
+        if context.mode != "SCULPT":
+            return False
+        obj = context.active_object
+        if not obj:
+            return False
+        if obj.type != "MESH":
+            return False
+
+        return True
+
+    def draw_header(self, context):
+        layout = self.layout
+
+        is_active = True
+        if not context.sculpt_object.use_dynamic_topology_sculpting:
+            is_active = False
+
+        layout.operator(
+            "sculpt.dynamic_topology_toggle",
+            text="Sculpt Sidekick Dyntopo",
+            icon="CHECKBOX_HLT" if is_active else "CHECKBOX_DEHLT",
+            emboss=True,
+        )
+
+    def draw(self, context):
+        sidekick = context.scene.sidekick_properties
+
+        sculpt = context.scene.tool_settings.sculpt
+
+        layout = self.layout
+
+        is_active = True
+        if not context.sculpt_object.use_dynamic_topology_sculpting:
+            is_active = False
+
+        method = layout.column()
+        method.active = is_active
+        method.prop(sculpt, "detail_type_method", expand=True)
+        cnst = layout.split(align=True, factor=0.9)
+        cnst.prop(sculpt, "constant_detail_resolution")
+        op_snpl = cnst.operator("sculpt.sample_detail_size", text="", icon="EYEDROPPER")
+        op_snpl.mode = "DYNTOPO"
+        rel = layout.column()
+        rel.prop(sculpt, "detail_size")
+        brush = layout.column()
+        brush.prop(sculpt, "detail_percent")
+        refine = layout.column()
+        refine.active = is_active
+        refine.prop(sculpt, "detail_refine_method", expand=True)
+        fill = layout.column()
+        fill.operator("sculpt.detail_flood_fill")
+        if (
+            sculpt.detail_type_method == "CONSTANT"
+            or sculpt.detail_type_method == "MANUAL"
+        ):
+            rel.active = False
+            brush.active = False
+        elif sculpt.detail_type_method == "RELATIVE":
+            cnst.active = False
+            brush.active = False
+        elif sculpt.detail_type_method == "BRUSH":
+            cnst.active = False
+            rel.active = False
+        if not is_active:
+            rel.active = False
+            cnst.active = False
+            brush.active = False
 
 
 class SculptSidekickRemeshPanel(bpy.types.Panel):
@@ -344,6 +364,7 @@ classList = (
     SculptSidekickFlipToolbarOp,
     SculptSidekickFlipSidebarOp,
     SculptSidekickPanel,
+    SculptSidekickDyntopoPanel,
     SculptSidekickRemeshPanel,
     SculptSidekickPieBrushMenu,
 )
