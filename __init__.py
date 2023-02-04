4e39301952bbc36251af45917edba563e64fd25d
bggd <gameovera@live.com>
2023-02-04 15:06:45 +0900

    bump version

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author": "birthggd",
     "description": "Sculpt Sidekick",
     "blender": (2, 80, 0),
-    "version": (0, 0, 3),
+    "version": (0, 0, 4),
     "location": "",
     "warning": "",
     "category": "3D View",
